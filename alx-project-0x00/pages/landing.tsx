18713edf028b769ff939332d98a6a48244a01f66
github.com/PRINCE-droid412 <Princetakyijnr@yahoo.com>
2025-07-16 02:22:44 -0400

    Button Props file updated

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import Card from "@/components/Card"
 import Pill from "@/components/Pill"
 import Button from "@/components/Button";
@@ -12,6 +13,21 @@ const Landing: React.FC =  () => {
         <Pill title="Villa" />
         <Pill title="apartment" />
         <Pill title="kitchen" />
+
+  <div className="flex gap-4 flex-wrap">
+    {/* Sizes */}
+    <Button title="Small Button" styles="text-sm px-3 py-1" />
+    <Button title="Medium Button" styles="text-base px-4 py-2" />
+    <Button title="Large Button" styles="text-lg px-6 py-3" />
+  </div>
+
+  <div className="flex gap-4 flex-wrap mt-4">
+    {/* Shapes */}
+    <Button title="Rounded Sm" styles="rounded-sm" />
+    <Button title="Rounded Md" styles="rounded-md" />
+    <Button title="Rounded Full" styles="rounded-full" />
+  </div>
+</div>
     </div>
 
     
